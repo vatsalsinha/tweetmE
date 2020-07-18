0688512706fb19c477a33cec4dd30e1941ac9cf3
Vatsal <vatsalsinha001@gmail.com>
2020-07-19 02:28:12 +0530

    19 July 2:28

--- tweetme2/urls.py
+++ tweetme2/urls.py
@@ -20,5 +20,6 @@ from tweets import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home_view),
-    path('tweet/<int:tweet_id>', views.tweet_detail_view)
+    path('tweet/<int:tweet_id>', views.tweet_detail_view),
+    path('tweets', views.tweet_list_view)
 ]
--- tweets/views.py
+++ tweets/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from .models import Tweet
 from django.http import HttpResponse, Http404, JsonResponse
-
+import random
 # Create your views here.
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context = {}, status = 200)
@@ -19,3 +19,12 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
         status = 404
    
     return JsonResponse(data, status = status )
+
+def tweet_list_view(request, *args, **kwargs):
+    qs = Tweet.objects.all()
+    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,12332) } for x in qs]
+    data = {
+        "isUser": False,
+        "response": tweets_list
+    }
+    return JsonResponse(data)
