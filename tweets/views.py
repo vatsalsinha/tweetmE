e8dada3f04d05741456ddce9748ac6cfdc9b6d7e
Vatsal <vatsalsinha001@gmail.com>
2020-07-22 02:25:50 +0530

    22 july 02:25

--- tweets/views.py
+++ tweets/views.py
@@ -29,7 +29,7 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0,12332) } for x in qs]
+    tweets_list = [x.serialize() for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
@@ -41,9 +41,15 @@ def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
     next_url = request.POST.get("next") or None
     if form.is_valid():
-        obj = form.save(commit = False)
+        obj = form.save(commit=False)
+        # do other form related logic
         obj.save()
-        if next_url is not None and is_safe_url(next_url, ALLOWED_HOSTS):
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) # 201 == created items
+        if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
-    return render(request, 'components/form.html', context= {"form": form})
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
+    return render(request, 'components/form.html', context={"form": form})
