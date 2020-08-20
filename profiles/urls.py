c54cc3a43060fadc0b139397488c47e7f6955e36
Vatsal <vatsalsinha001@gmail.com>
2020-08-20 21:04:49 +0530

    20 aug 2020, 21:04

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import profile_detail_view
+from .views import profile_detail_view, profile_update_view
 
 urlpatterns = [
-    path('<str:username>', profile_detail_view)
+    path('<str:username>', profile_detail_view),
+    path('edit/', profile_update_view),
 ]
