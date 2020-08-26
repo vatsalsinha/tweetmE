a09ad6b19cf68974c19c8243834b916b796a70f8
Vatsal <vatsalsinha001@gmail.com>
2020-08-26 17:23:54 +0530

    26 aug 2020, 17:23

--- tweetme2-web/src/tweets/list.js
+++ tweetme2-web/src/tweets/list.js
@@ -5,6 +5,7 @@ import {apiTweetList} from './lookup'
 export function TweetsList(props) {
     const [tweetsInit, setTweetsInit] = useState([])
     const [tweets, setTweets] = useState([])
+    const [nextUrl, setNextUrl] = useState(null)
     const [tweetsDidSet, setTweetsDidSet] = useState(false)
     useEffect(()=>{
       const final = [...props.newTweets].concat(tweetsInit)
@@ -17,7 +18,8 @@ export function TweetsList(props) {
       if (tweetsDidSet === false){
         const handleTweetListLookup = (response, status) => {
           if (status === 200){
-            setTweetsInit(response)
+            setNextUrl(response.next)
+            setTweetsInit(response.results)
             setTweetsDidSet(true)
           } else {
             alert("There was an error")
@@ -34,7 +36,27 @@ export function TweetsList(props) {
          updateFinalTweets.unshift(tweets)
          setTweets(updateFinalTweets)
     }
-    return tweets.map((item, index)=>{
+
+    const handleLoadNext = (event) => {
+      event.preventDefault()
+      if (nextUrl !== null) {
+        const handleLoadNextResponse = (response, status) =>{
+          if (status === 200){
+            setNextUrl(response.next)
+            const newTweets = [...tweets].concat(response.results)
+            setTweetsInit(newTweets)
+            setTweets(newTweets)
+          } else {
+            alert("There was an error")
+          }
+        }
+        apiTweetList(props.username, handleLoadNextResponse, nextUrl)
+      }
+    }
+
+    return <React.Fragment> {tweets.map((item, index)=>{
       return <Tweet tweet={item} didRetweet = {handleDidRetweet} className='my-5 py-5 border bg-white text-dark' key={`${index}-{item.id}`} />
-    })
+    })}
+    {nextUrl !== null && <button onClick = {handleLoadNext} className = 'btn btn-outline-primary'>Next</button>}
+    </React.Fragment>
   }
--- tweetme2-web/src/tweets/lookup.js
+++ tweetme2-web/src/tweets/lookup.js
@@ -12,10 +12,13 @@ export function apiTweetDetail(tweetId, callback) {
     backendLookup("GET", endpoint, callback)
 }
   
-export function apiTweetList(username, callback) {
+export function apiTweetList(username, callback, nextUrl) {
     let endpoint = "/tweets/"
     if (username){
         endpoint = `/tweets/?username=${username}`
+    }
+    if (nextUrl !== null && nextUrl !== undefined){
+        endpoint = nextUrl.replace("http://localhost:8000/api", "")
     }
     backendLookup("GET", endpoint, callback)
 }
