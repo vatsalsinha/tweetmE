156efde2561ab1a0261c9935b6ae7e5a95e8deb5
Vatsal <vatsalsinha001@gmail.com>
2020-09-04 16:49:12 +0530

    sept 4 2020 16:48 -> Follow button

--- profiles/api/views.py
+++ profiles/api/views.py
@@ -53,6 +53,6 @@ def user_follow_view(request, username, *args, **kwargs):
         profile.followers.remove(me)
     else:
         pass
-    current_followers_qs = profile.followers.all()
-    return Response({"count": current_followers_qs.count()}, status = 200) 
+    data = PublicProfileSerializer(instance = profile, context = {"request": request})
+    return Response(data.data, status = 200) 
 
--- tweetme2-web/src/profiles/components.js
+++ tweetme2-web/src/profiles/components.js
@@ -13,12 +13,12 @@ export function UserLink(props){
   }
   
 export function UserDisplay(props){
-    const {user, includeFullName} = props
+    const {user, includeFullName, hideLink} = props
     const nameDisplay = includeFullName === true ? `${user.first_name} ${user.last_name}` : null;
     return (
       <React.Fragment>
         {nameDisplay}{' '}
-        <UserLink username = {user.username} >
+        {hideLink === true ? `@${user.username}` : <UserLink username = {user.username} >
           @{user.username}{' '} 
           <span className = 'small text-muted'>
             <br/>
@@ -27,16 +27,15 @@ export function UserDisplay(props){
             <span className="glyphicon glyphicon-map-marker" aria-hidden="true">{user.location}</span>
             <br />
           </span>
-        </UserLink>
+        </UserLink>}
       </React.Fragment>
     )
   }
   
 export function UserPicture(props){
-    const {user} = props
-    return  <UserLink username = {user.username} >
-      <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
-        {user.username[0]}
-      </span>
-    </UserLink>
+    const {user, hideLink} = props
+    const userIdSpan =  <span className='mx-1 px-3 py-2 rounded-circle bg-dark text-white'>
+    {user.username[0]}
+  </span>
+    return  hideLink === true ? userIdSpan : <UserLink username = {user.username} > {userIdSpan}</UserLink>
   }
